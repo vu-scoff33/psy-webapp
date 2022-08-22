85e8c116a35c4950e58f65fb7b5816c72a3be79e
Vu Le <lev@rotini.union.edu>
2022-08-22 12:42:31 -0400

    ok

--- static/public/participant.js
+++ static/public/participant.js
@@ -1,3 +1,7 @@
 document.querySelectorAll('canvas').forEach(elem => {
   DrawingCanvas(elem);
 })
+
+document.querySelector('form#survey-fillout').onsubmit = function(event){
+  event.preventDefault();
+}
